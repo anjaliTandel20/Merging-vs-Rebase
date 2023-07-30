a2ef7ba3897161eae0f19b911f9a74f9a8c38d3e
Anjali Tandel <anjali.tandel@1rivet.local>
2023-07-30 21:24:20 +0530

    Display git merge title & content

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Merging-vs-Rebase';
   
+  public gitMerge: string = '';
+  public gitMergeContent: string = '';
+
+  public ngOnInit(): void {
+    this.initValues();
+  }
+
+  initValues() {
+    this.gitMerge = 'What is Git Rebase';
+    this.gitMergeContent = 'Git rebase is a command that lets users integrate changes from one branch to another, and the logs are modified once the action is complete. Git rebase was developed to overcome merging’s shortcomings, specifically regarding logs.'
+  }
 }
