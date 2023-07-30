5532c4630e0d265ed01d8292f79e923bc6926af4
Anjali Tandel <anjali.tandel@1rivet.local>
2023-07-30 21:24:20 +0530

    Display git merge title & content

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent {
   title = 'Merging-vs-Rebase';
   public gitReset: string = '';
   public gitResetContent: string = '';
+  
+  public gitMerge: string = '';
+  public gitMergeContent: string = '';
 
   public ngOnInit(): void {
     this.initValues();
@@ -17,5 +20,7 @@ export class AppComponent {
   initValues() {
     this.gitReset = 'What’s a Git Reset?';
     this.gitResetContent = 'Git reset is a powerful command that undoes changes, moving the repository back to a previous commit, and eliminating any changes made after the commit. It’s literally hitting the “reset” button, undoing local changes.'
+    this.gitMerge = 'What is Git Rebase';
+    this.gitMergeContent = 'Git rebase is a command that lets users integrate changes from one branch to another, and the logs are modified once the action is complete. Git rebase was developed to overcome merging’s shortcomings, specifically regarding logs.'
   }
 }
