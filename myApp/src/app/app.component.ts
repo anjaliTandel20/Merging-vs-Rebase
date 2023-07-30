10e823579421aa5e5d25d823d203c12ce24405f9
Anjali Tandel <anjali.tandel@1rivet.local>
2023-07-30 21:21:24 +0530

    Display git rebase title & content

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -12,6 +12,8 @@ export class AppComponent {
   
   public gitMerge: string = '';
   public gitMergeContent: string = '';
+  public gitRebase: string = '';
+  public gitRebaseContent: string = '';
 
   public ngOnInit(): void {
     this.initValues();
@@ -22,5 +24,7 @@ export class AppComponent {
     this.gitResetContent = 'Git reset is a powerful command that undoes changes, moving the repository back to a previous commit, and eliminating any changes made after the commit. It’s literally hitting the “reset” button, undoing local changes.'
     this.gitMerge = 'What is Git Rebase';
     this.gitMergeContent = 'Git rebase is a command that lets users integrate changes from one branch to another, and the logs are modified once the action is complete. Git rebase was developed to overcome merging’s shortcomings, specifically regarding logs.'
+    this.gitRebase = 'What is Git Rebase';
+    this.gitRebaseContent = 'Git rebase is a command that lets users integrate changes from one branch to another, and the logs are modified once the action is complete. Git rebase was developed to overcome merging’s shortcomings, specifically regarding logs.'
   }
 }
