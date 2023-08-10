1d0178f0be433d0ab3e8c2e266fe07934534bb0d
anjaliTandel20 <50238696+anjaliTandel20@users.noreply.github.com>
2023-08-10 11:35:58 +0530

    Main Commit 5

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title: string = 'Merge branch commit';
+  public title: string = 'Merge branch commits';
 }
